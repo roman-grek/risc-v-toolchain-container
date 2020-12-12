7118daf56ccb77be7b20767b85082672ec91b48f
Roman Grek <roman.grek0098@gmail.com>
2020-12-13 00:32:36 +0300

    Update dockerfiles

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import subprocess
 
-default_command = ['', '']
+command_args = ""
+commit = "master"
+arch = "rv64imac"
 
 specify_commit = input("Specify commit? y/n ")
 if specify_commit == "y":
@@ -9,10 +11,16 @@ else:
     specify_branch = input("Specify branch? y/n ")
     if specify_branch == "y":
         commit = input("Branch name: ")
-    else:
-        commit = "master"
 
+specify_arch = input(f"Specify architecture ({arch} by default)? y/n ")
+if specify_arch == "y":
+    arch = input("Arch: ")
 
+add_args = input (f"Current command is `./configure--with-arch=${arch}`. Want to add args? y/n ")
+if add_args == "y":
+    command_args = input("Additional args: ")
 
-list_files = subprocess.run(["echo", commit])
-print("The exit code was: %d" % list_files.returncode)
+with open(".env", "w") as env_file:
+    env_file.writelines([f'RISCV_COMMIT="{commit}"\n', f'RISCV_ARGS="{command_args}"\n',  f'RISCV_ARCH="{arch}"'])
+
+subprocess.run(["docker-compose", "up"])
--- simulator/test.sh
+++ simulator/test.sh
@@ -1,12 +1,13 @@
 #!bin/bash
-files=$(ls /tests/benchmarks -a | grep  "\.riscv$")
-for file in $files ; do
+
+riscv_files=$(ls /tests/benchmarks -a | grep  "\.riscv$")
+for file in $riscv_files ; do
   echo "Executing ${file}..." > "/tests/results_riscv.txt"
-  /mipt-mips/build/mipt-mips -b /tests/benchmarks/$file --isa riscv64 -d > "/tests/results_riscv.txt"    
+  /mipt-mips/build/mipt-mips -b /tests/benchmarks/$file --isa riscv64 -d >> "/tests/results_riscv.txt"    
 done
 
 isa_files=$(ls /tests/benchmarks -a | grep  "[^\.dump]$")
 for file in $isa_files ; do
   echo "Executing ${file}..." > "/tests/results_isa.txt"
-  /mipt-mips/build/mipt-mips -b /tests/isa/$file --isa riscv64 -n 800 > "/tests/results_isa.txt"     
+  /mipt-mips/build/mipt-mips -b /tests/isa/$file --isa riscv64 -n 800 >> "/tests/results_isa.txt"     
 done
