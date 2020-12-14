ef9812db14618843eb13ed4d2123ddc3dde3bce1
Roman Grek <roman.grek0098@gmail.com>
2020-12-14 13:18:10 +0300

    Fix docker-compose issues

--- simulator/test.sh
+++ simulator/test.sh
@@ -3,11 +3,11 @@
 riscv_files=$(ls /tests/benchmarks -a | grep  "\.riscv$")
 for file in $riscv_files ; do
   echo "Executing ${file}..." > "/tests/results_riscv.txt"
-  /mipt-mips/build/mipt-mips -b /tests/benchmarks/$file --isa riscv64 -d >> "/tests/results_riscv.txt"    
+  /build/mipt-mips -b /tests/benchmarks/$file --isa riscv64 -d >> "/tests/results_riscv.txt"    
 done
 
 isa_files=$(ls /tests/benchmarks -a | grep  "[^\.dump]$")
 for file in $isa_files ; do
   echo "Executing ${file}..." > "/tests/results_isa.txt"
-  /mipt-mips/build/mipt-mips -b /tests/isa/$file --isa riscv64 -n 800 >> "/tests/results_isa.txt"     
+  /build/mipt-mips -b /tests/isa/$file --isa riscv64 -n 800 >> "/tests/results_isa.txt"     
 done
